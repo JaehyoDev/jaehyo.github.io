0bf32136d732b9ea6255f5017ba2c9a5572e5929
JH <67933778+jayhigh47@users.noreply.github.com>
2022-11-19 15:47:13 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener("scroll", () => {
-  console.log("window.scrollY:" + window.scrollY);
+  //console.log("window.scrollY:" + window.scrollY);
   //console.log("navbarHeight:" + navbarHeight);
   console.log(`navbarHeight: ${navbarHeight}`);
   if (window.scrollY > navbarHeight) {
@@ -19,13 +19,13 @@ document.addEventListener("scroll", () => {
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector(".navbar__menu");
 navbarMenu.addEventListener("click", (event) => {
-  console.log(event.target);
+  //console.log(event.target);
   const target = event.target;
   const link = target.dataset.link;
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link); // html파일에서 data-link를 불러옴
+  //console.log(event.target.dataset.link); // html파일에서 data-link를 불러옴
   scrollIntoView(link);
 });
 
@@ -39,3 +39,12 @@ function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector(".home__container");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  //console.log(homeHeight);
+  //console.log(1 - window.scrollY / homeHeight);
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
