4cd4372260583ead0a3d034ed05d7b8c9e6f3233
JH <67933778+jayhigh47@users.noreply.github.com>
2022-11-20 02:50:24 +0900

    JS: Project filter

--- main.js
+++ main.js
@@ -59,55 +59,6 @@ arrowUp.addEventListener("click", () => {
   scrollIntoView("#home");
 });
 
-// Projects
-const workBtnContainer = document.querySelector(".work__categories");
-const projectContainer = document.querySelector(".work__projects");
-const projects = document.querySelectorAll(".project");
-workBtnContainer.addEventListener("click", (e) => {
-  //const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-  const filter = e.target.dataset.filter; // 이렇게 필터하면 버튼안에 스팬이 클릭되면 감지되지 않음 이상함...
-  if (filter == null) {
-    return;
-  }
-  //console.log(filter);
-  projectContainer.classList.add("anim-out");
-
-  //setTimeout은 브라우저의 api, 0.3초 후 발동
-  setTimeout(() => {
-    projects.forEach((project) => {
-      //console.log(project.dataset.type);
-
-      if (filter == "*" || filter == project.dataset.type) {
-        project.classList.remove("invisible");
-      } else {
-        project.classList.add("invisible");
-      }
-    });
-    projectContainer.classList.remove("anim-out");
-  }, 300);
-
-  // // 1번 방법
-  // projects.forEach((project) => {
-  //   console.log(project);
-  // });
-
-  // console.log("--------------------");
-
-  // // 2번 방법
-  // for (let project of projects) {
-  //   console.log(project);
-  // }
-
-  // console.log("--------------------");
-
-  // // 3번 방법
-  // let project;
-  // for (let i = 0; i < projects.length; i++) {
-  //   project = projects[i];
-  //   console.log(project);
-  // }
-});
-
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: "smooth" });
